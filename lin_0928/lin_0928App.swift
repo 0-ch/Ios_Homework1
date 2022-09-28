151b2597156fb3217167d0c8d1d626c19a2391d8
tony <usengjake@gmail.com>
2022-09-28 14:15:41 +0800

    Initial Commit

---
+++ lin_0928/lin_0928App.swift
@@ -0,0 +1,17 @@
+//
+//  lin_0928App.swift
+//  lin_0928
+//
+//  Created by User01 on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct lin_0928App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
